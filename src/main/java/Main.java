969514cf1a7d2db72e5fec2812eaf5b298ca9cfa
tomaszlach86 <tomaszlach86@gmail.com>
2019-04-28 19:10:57 +0200

    Pierwsza Wersja drukowania drzewka

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
+        printTree();
+    }
+
+    static void printTree(){
+        for(int i = 1; i<=4; i++){
+            for(int j = 0; j<4-i;j++){
+                printSpaces();
+            }
+            for(int k =1; k<=2*i-1; k++){
+                printStars();
+            }
+            System.out.println("");
+        }
+        //drukowanie pnia wyrzuc do innej metody
+        //sparametryzuj drzewko :)
+
+        for(int i = 1; i<=3;i++){
+            printSpaces();
+        }
+        printStars();
+        System.out.println("");
+
+        for(int i = 1; i<=3;i++){
+            printSpaces();
+        }
+        printStars();
+        System.out.println("");
+    }
+
+    static void printStars(){
+        System.out.print("*");
+    }
+
+    static void printSpaces(){
+        System.out.print(" ");
     }
 }

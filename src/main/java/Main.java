5a70314f514df7287e9077a878b7ee246d6aaf1a
tomaszlach86 <tomaszlach86@gmail.com>
2019-04-28 19:38:16 +0200

    Refactor kodu - parametryzacja metody printTree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,47 +3,49 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
         printTree();
+        //printTree(2,2);
+        //printTree(3,2);
+        //printTree(10,10);
     }
 
-    private static void printTree(){
-        //sparametryzuj drzewko :)
-        printCrown();
-        printTrunk();
+    private static void printTree() {
+        printTree(4, 2);
     }
 
-    private static void printTrunk() {
-        for(int i = 1; i<=3;i++){
-            printSpaces();
-        }
-        printStars();
-        System.out.println();
+    private static void printTree(int crownSize, int trunkSize) {
 
-        for(int i = 1; i<=3;i++){
-            printSpaces();
-        }
-        printStars();
-        System.out.println();
+        printCrown(crownSize);
+        printTrunk(crownSize, trunkSize);
     }
 
-    private static void printCrown() {
+    private static void printCrown(int crownSize) {
 
-        for(int i = 1; i<=4; i++){
-            for(int j = 0; j<4-i; j++){
+        for (int i = 1; i < crownSize+1; i++) {
+            for (int j= 0; j < crownSize - i+1; j++) {
                 printSpaces();
             }
-            for(int k =1; k<=2*i-1; k++){
+            for (int k = 0; k < 2 * i - 1; k++) {
                 printStars();
             }
             System.out.println();
         }
     }
 
+    private static void printTrunk(int crownSize, int trunkSize) {
+        for (int i = 0; i < trunkSize; i++) {
+            for (int j = 1; j < crownSize +1 ; j++) {
+                printSpaces();
+            }
+            printStars();
+            System.out.println();
+        }
+    }
 
-    private static void printStars(){
+    private static void printStars() {
         System.out.print("*");
     }
 
-    private static void printSpaces(){
+    private static void printSpaces() {
         System.out.print(" ");
     }
 }

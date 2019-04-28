755e6d3fc6408f92dae37e53bc4727d375c99824
tomaszlach86 <tomaszlach86@gmail.com>
2019-04-28 19:08:19 +0200

    Zmiana wg kroku 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Uczę się programowania");
+        System.out.println("Uczę się programowania i commitowania");
     }
 }
